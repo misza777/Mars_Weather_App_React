b5d385d82926cc5e3f522c8c88616e6176769de8
misha novakishvili <46324818+misza777@users.noreply.github.com>
2020-06-02 17:19:33 +0200

    Updates

---
+++ precache-manifest.9904d2cdf38b3717350aca1b954963ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c58d0f74b512542557ab029ca0aa6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "103cdc5d638e7f7a9f84",
+    "url": "/static/js/2.242cf7c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.242cf7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac4994e8b28ca914baa",
+    "url": "/static/js/main.a0e1b258.chunk.js"
+  },
+  {
+    "revision": "8ec9df1264a8cbd5de8d",
+    "url": "/static/js/runtime-main.a9536e54.js"
+  },
+  {
+    "revision": "249d1f5c2f3de6ea11d25fa99ce4261d",
+    "url": "/static/media/nasa-mars-bg.249d1f5c.jpg"
+  }
+]);
