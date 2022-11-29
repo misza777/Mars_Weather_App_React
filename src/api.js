b2d6a8870adc9bc4e6b82e94d52856fb82e7570a
micha novakishvili <46324818+misza777@users.noreply.github.com>
2022-11-29 12:13:38 +0300

    Update api.js
    new api key

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-const API_KEY = "FqvvYsou3HmZcttWCDCehCvqkKOLKnaO1TUyaqcS";
+const API_KEY = "LonoXWQJ1drhfOfR054f1E5jmbY0rpqkQlQeRGtX";
 export const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
